b35d63ff80ff27abee42f914cc80203714a055ba
Braindawgs <andrejkljajopetkovic@gmail.com>
2024-08-28 13:05:48 +0200

    bug: try catch added to main execution

--- main.cpp
+++ main.cpp
@@ -5,82 +5,100 @@
 
 int main()
 {
-    SmartStorage storage(std::make_unique<Database>());
+    std::unique_ptr<SmartStorage> storage;
 
-    int choice;
-    int key = 1;
-    do 
+    try
     {
-        std::cout << "--------------" << std::endl;
-        std::cout << "Options:\n";
-        std::cout << "1) Write\n";
-        std::cout << "2) Read\n";
-        std::cout << "3) List all entries\n";
-        std::cout << "4) Corrupt\n"; 
-        std::cout << "5) Quit\n";
-        std::cout << "--------------" << std::endl;
-        std::cin >> choice;
+        storage = std::make_unique<SmartStorage>(std::make_unique<Database>());
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::cout << "Exiting due to a failure..." << std::endl;
+        exit(1);
+    }
 
-        // Error safety
-        if (std::cin.fail())
+    try
+    {
+        int choice;
+        int key = 1;
+        do 
         {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        }
+            std::cout << "--------------" << std::endl;
+            std::cout << "Options:\n";
+            std::cout << "1) Write\n";
+            std::cout << "2) Read\n";
+            std::cout << "3) List all entries\n";
+            std::cout << "4) Corrupt\n"; 
+            std::cout << "5) Quit\n";
+            std::cout << "--------------" << std::endl;
+            std::cin >> choice;
 
-        switch (choice)
-        {
-            case 1:
+            // Error safety
+            if (std::cin.fail() || (choice < 1) || (choice > 5))
             {
-                std::string txt;
-                std::cout << "Enter text:";
-                std::cin.ignore();
-                std::getline(std::cin, txt);
-
-                storage.write(key, txt);
-                std::cout << "Text:\"" << txt << "\" is saved on position " << key << std::endl;
-                key++;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             }
-            break;
-            case 2:
-            {
-                int row;
-                std::cout << "Enter row to be read:";
-                std::cin >> row;
 
-                try
+            switch (choice)
+            {
+                case 1:
                 {
-                    std::cout << storage.read(row) << std::endl;
+                    std::string txt;
+                    std::cout << "Enter text:";
+                    std::cin.ignore();
+                    std::getline(std::cin, txt);
+
+                    storage->write(key, txt);
+                    std::cout << "Text:\"" << txt << "\" is saved on position " << key << std::endl;
+                    key++;
                 }
-                catch(const std::exception& e)
+                break;
+                case 2:
                 {
-                    
-                    std::cerr << "Error: " << e.what() << std::endl;
+                    int row;
+                    std::cout << "Enter row to be read:";
+                    std::cin >> row;
+
+                    try
+                    {
+                        std::cout << storage->read(row) << std::endl;
+                    }
+                    catch(const std::exception& e)
+                    {
+                        
+                        std::cerr << "Error: " << e.what() << std::endl;
+                    }
                 }
+                break;
+                case 3:
+                {
+                    storage->listAll();
+                }
+                break;
+                case 4:
+                {
+                    int row;
+                    std::cout << "Which row to corrupt:";
+                    std::cin >> row;
+                    storage->corruptor(row, "CoRRuPtiiiiNg:!11!230###21");
+                }
+                break;
+                case 5:
+                {
+                    std::cout << "Exiting..." << std::endl;
+                }
+                break;
+                default:
+                    std::cerr << "Non valid option selected" << std::endl;
             }
-            break;
-            case 3:
-            {
-                storage.listAll();
-            }
-            break;
-            case 4:
-            {
-                int row;
-                std::cout << "Which row to corrupt:";
-                std::cin >> row;
-                storage.corruptor(row, "CoRRuPtiiiiNg:!11!230###21");
-            }
-            break;
-            case 5:
-            {
-                std::cout << "Exiting..." << std::endl;
-            }
-            break;
-            default:
-                std::cerr << "Non valid option selected" << std::endl;
-        }
-    } while(5 != choice);
+        } while(5 != choice);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
     
     return 0;
 }
